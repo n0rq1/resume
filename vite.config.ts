36eb55ca8d0717d7b6bc58f12116cb9c52cd2a14
Austin Norquist <austin.norquist@liatrio.com>
2025-07-05 09:39:35 -0700

    Update config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/resume/',
   plugins: [react()],
   server: {
     port: 5175,
     strictPort: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
