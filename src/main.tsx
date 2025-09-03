fde0256b8f4287a244dda82332b5263ec7cb4f72
Austin Norquist <austin.norquist@liatrio.com>
2025-09-03 07:06:37 -0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import coolPicture from './assets/cool-picture.png';
+
+function ensureFavicon() {
+  try {
+    const doc = document;
+    let link = doc.querySelector("link[rel='icon']") as HTMLLinkElement | null;
+    if (!link) {
+      link = doc.createElement('link');
+      link.rel = 'icon';
+      doc.head.appendChild(link);
+    }
+    link.type = 'image/png';
+    link.href = coolPicture; // Vite will resolve to hashed asset URL in production
+
+    // Also set shortcut icon for broader browser support
+    let shortcut = doc.querySelector("link[rel='shortcut icon']") as HTMLLinkElement | null;
+    if (!shortcut) {
+      shortcut = doc.createElement('link');
+      shortcut.rel = 'shortcut icon';
+      doc.head.appendChild(shortcut);
+    }
+    shortcut.type = 'image/png';
+    shortcut.href = coolPicture;
+  } catch (e) {
+    // Non-fatal; favicon is cosmetic
+    console.warn('Failed to set favicon', e);
+  }
+}
+
+ensureFavicon();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
